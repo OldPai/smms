53b278aa1c8af0de6e4a9568d2763d10b12e8b3a
admin <admin@163.com>
2018-09-28 21:58:52 +0800

    完成了用户管理员修改密码功能和登录、退出验证功能

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,69 @@ connection.connect(() => {
   console.log('与数据库admin连接成功...')
 });
 
+
+/* 登录请求：检查用户名和密码是否正确 */
+router.post('/checkLogin', (req, res) => {
+  // 接收用户名和密码
+  let { username, password } = req.body;
+
+  // 构造sql语句 查询接收的用户名和密码是否存在  （用户名和密码是且的关系）
+  const sqlStr = `select * from users where username='${username}' and password='${password}'`;
+  // console.log(sqlStr);
+  // 执行sql语句（根据前端发送过来的数据查询用户名和密码是否存在）
+  connection.query(sqlStr, (err, data) => {
+    if (err) {
+      // 如果有错 抛出错误
+      throw err
+    } else {
+      // 判断：如果存在，返回成功的数据对象
+      if (data.length) {
+        console.log(data);
+        // 登录成功，设置cookie (在res.send()之前设置)
+        res.cookie('username', data[0].username);
+        res.cookie('password', data[0].password);
+        res.cookie('groups', data[0].groups);
+
+        // 返回成功的数据对象
+        res.send({'errcod':1,'msg':'登录成功！！！'});
+      } else {
+        // 否则  用户不存在  返回失败的数据对象
+        res.send({'errcod':0,'msg':'请检查用户名或密码是否正确！！！'});
+      }
+    }
+  });
+});
+
+/* 检查用户是否已经登录 */
+router.get('/checkIsLogin', (req, res) => {
+  // 从浏览器获取cookie（看一下有没有，如果有 就是登录过的 如果没有 就是直接进来的没有登录的）
+  // console.log('用户名',req.cookies.username);
+
+  // 从浏览器获取cookie
+  let username = req.cookies.username
+  // 如果存在 随便响应一些信息（不影响用户操作）
+  if (username) {
+    res.send('console.log("")');
+  } else {
+    // 如果不存在 就是没有登录  弹出对应提示  并跳转到登录页面
+    res.send('alert("请登录以后再操作..."); location.href="./login.html";');
+  }
+});
+
+/* 退出登录 */
+router.get('/logout', (req, res) => {
+  // 清除浏览器cookie
+  res.clearCookie('username');
+  res.clearCookie('password');
+  res.clearCookie('groups');
+
+  // 弹出对应提示  跳转到登录页面
+  res.send('<script>alert("退出成功！！！"); location.href="http://localhost:2345/login.html"</script>');
+});
+/*  */
+/*  */
+/*  */
+/*  */
 /* 接收添加用户账号的请求 */
 router.post('/userAdd', (req, res) => {
   // 接收前端发送给后端的账号数据
@@ -57,7 +120,6 @@ router.get('/userList', (req, res) => {
 });
 
 
-
 /* 接收单条删除的请求 */
 router.get('/userDelOne', (req, res) => {
   // 接收前端发送过来要删除的数据  对应的id
@@ -83,6 +145,87 @@ router.get('/userDelOne', (req, res) => {
   })
 });
 
+
+/* 接收修改用户账号数据的请求：把原来的数据查询出来，返回给前端 */
+router.get('/userEdit', (req, res) => {
+  // 接收前端传过来的id
+  let { id } = req.query;
+  // console.log(id);
+
+  // 根据接收到的id构造sql语句（在数据库里查询这条数据）
+  const sqlStr = `select * from users where id=${id}`;
+  // 执行sql语句(用构造的sql语句在数据库里查询这条数据)
+  connection.query(sqlStr, (err, data) => {
+    // 如果有错  抛出错误
+    if (err) {
+      throw err;
+    } else {
+      // 否则  把根据id查询到的这条数据（要修改的数据）响应给前端
+      res.send(data)
+    }
+  });
+});
+
+
+/* 接收修改用户账号数据的请求：把修改后的数据保存会mysql数据库 并覆盖原来的数据 */
+router.post('/saveEdit', (req, res) => {
+  // 接收前端发送过来的数据
+  let { username, password, groups, id } = req.body;
+
+  // 构造sql语句（根据id  修改这条数据）
+  const sqlStr = `update users set username='${username}',password='${password}',groups='${groups}' where id=${id}`;
+  // console.log(sqlStr);
+  // 执行修改用户数据的sql语句
+  connection.query(sqlStr, (err, data) => {
+    // 如果有错  抛出错误
+    if (err) {
+      throw err;
+    } else {  // 否则  响应数据给前端
+      // 判断: 如果受影响行数 > 1  就是修改成功   否则 就是修改失败
+      if (data.affectedRows > 0) {
+        // 响应修改成功的数据给前端
+        res.send({ "errcode": 1, "msg": "修改成功！" });
+      } else {
+        // 响应修改失败的数据给前端
+        res.send({ "errcode": 0, "msg": "修改失败！" });
+      }
+    }
+  });
+});
+
+
+/* 接收批量删除的请求 （得到的是前端发送过来的一个要删除数据 id 组成的数组） */
+router.post('/batchesDel', (req, res) => {
+  // 接收前端选中的需要批量删除的数据的 id 
+  let idArr = req.body['idArr[]'];
+
+  // 构造sql语句：根据接收到的这些id  把这些被选中的数据全部删除
+  const sqlStr = `delete from users where id in (${idArr})`;
+  // console.log(sqlStr);
+
+  // 执行构造的批量删除sql语句
+  connection.query(sqlStr, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      // 判断：如果受影响行数 > 0 （批量删除几个就是几行）就是删除成功  返回成功的数据对象
+      if (data.affectedRows > 0) {
+        res.send({ 'errcode': 1, 'msg': '批量删除成功！！' });
+      } else {
+        // 否则  就是批量删除失败  返回失败的数据对象
+        res.send({ 'errcode': 0, 'msg': '批量删除失败！！' });
+      }
+    }
+  });
+});
+
+
+/*  */
+/*  */
+/*  */
+/*  */
+/*  */
+/*  */
 /*  */
 /*  */
 /*  */
